1e3f7fa620e7b1ccbe6177ae178ae0027a18763a
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-07-09 00:08:35 -0300

    chore: Update site title and tagline, and translate feature titles and descriptions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,10 +58,10 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Polvo π',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/apple-touch-icon.png',
       },
       items: [
         {
